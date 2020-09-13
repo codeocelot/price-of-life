ca6e86c5c0cb881e4e574fbcf8c8b9800291211d
Joey Gracey <joey@yewno.com>
2020-09-13 14:25:35 -0700

    Updates

---
+++ precache-manifest.e6b6de7ea6a8b4086877a934ad98cc30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed60c027a4c5a6b62067474fc704b4f",
+    "url": "/price-of-life/index.html"
+  },
+  {
+    "revision": "a24b8b272eaafd5cfc53",
+    "url": "/price-of-life/static/css/main.2d5c788b.chunk.css"
+  },
+  {
+    "revision": "1030018fdeef0a286d33",
+    "url": "/price-of-life/static/js/2.ccf450bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/price-of-life/static/js/2.ccf450bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24b8b272eaafd5cfc53",
+    "url": "/price-of-life/static/js/main.589912dd.chunk.js"
+  },
+  {
+    "revision": "b94473f20b77965828f0",
+    "url": "/price-of-life/static/js/runtime-main.e11cbe0d.js"
+  }
+]);
